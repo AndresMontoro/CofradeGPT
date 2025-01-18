f7e2895152231edaed75e2516e64939bd2ef76a2
AndresMontoro <andres.montorovenegas@alum.uca.es>
2025-01-18 19:44:50 +0100

    Nuevo modulo: Backoffice

---
+++ backoffice/src/main/java/es/ca/andresmontoro/backoffice/BackofficeApplication.java
@@ -0,0 +1,13 @@
+package es.ca.andresmontoro.backoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackofficeApplication.class, args);
+	}
+
+}
