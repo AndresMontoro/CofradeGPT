d35babb534aa055e3ffcde7384703e4551dc1f7d
AndresMontoro <andres.montorovenegas@alum.uca.es>
2025-01-25 18:56:29 +0100

    Nueva security config

--- backoffice/src/main/java/es/ca/andresmontoro/backoffice/shared/infrastructure/SecurityConfig.java
+++ backoffice/src/main/java/es/ca/andresmontoro/backoffice/shared/infrastructure/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.security.web.util.matcher.RequestMatcher;
 
 @Configuration
 public class SecurityConfig {
